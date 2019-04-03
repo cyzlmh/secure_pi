e22e764b8713a754c8333d76508a9a5d87c95116
pi-yzchen <cyzlmh@gmail.com>
2019-04-04 02:10:40 +0800

    release v1.0.0

--- motion_detector.py
+++ motion_detector.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 import picamera
 import cv2
 import numpy as np
-import imutils
+from smb.SMBConnection import SMBConnection
 
 MIN_AREA = 100
 WIDTH = 640
@@ -13,6 +13,12 @@ email_freq = 30*60
 sample_rate = 3
 storage_path = '/home/pi/Pictures'
 prior_image = None
+
+server_ip = '192.168.3.100'
+user = 'raspi'
+password = 'raspberry-90'
+client_name = 'pi'
+server_name = 'h100'
 
 def ts_str():
     return datetime.now().strftime('%Y%m%d-%H%M%S')
@@ -29,17 +35,18 @@ def diff(frame1, frame2):
     
     # dilate the thresholded image to fill in holes, then find contours on thresholded image
     thresh = cv2.dilate(thresh, None, iterations=2)
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1]
     objs = []
     for c in cnts:
         if cv2.contourArea(c) >= MIN_AREA:
             objs.append(cv2.boundingRect(c))
+    if len(objs) > 0:
+        cv2.imwrite('/home/pi/Pictures/'+ts_str()+'-t.jpg', thresh)
     return objs
 
 def stream_to_gray(stream):
     image = np.frombuffer(stream.read(), dtype=np.uint8).reshape(HIGTH, WIDTH, 3)
-    image = imutils.resize(image, width=500)
+    #image = imutils.resize(image, width=500)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = cv2.GaussianBlur(image, (21, 21), 0)
     return image
@@ -72,6 +79,10 @@ if __name__ == '__main__':
         camera.wait_recording(2)
         last_detect = datetime.now() - timedelta(0, email_freq)
         try:
+            conn = SMBConnection(user, password, client_name, server_name)
+            conn.connect(server_ip, timeout=30)
+            share = conn.listShares()[1]
+            print('smb server connected')
             while True:
                 wait(camera, 3)
                 if detect_motion(camera):
@@ -79,25 +90,28 @@ if __name__ == '__main__':
                     prefix = os.path.join(storage_path, ts_str())
                     camera.capture(prefix+'.jpg', use_video_port=True)
                     if datetime.now() - last_detect > timedelta(0, email_freq):
-                        os.system('python3 /home/pi/scripts/send_email.py anto_nozomi@126.com '\
-                            +ts_str()+'-motion-detect null')
+                        #os.system('python3 /home/pi/scripts/send_email.py anto_nozomi@126.com '\
+                        #    +ts_str()+'-motion-detect null')
                         last_detect = datetime.now()
                     # As soon as we detect motion, split the recording to
                     # record the frames "after" motion
-                    camera.split_recording(prefix+'-after.h264')
                     # Write the 10 seconds "before" motion to disk as well
-                    #write_video(stream)
-                    stream.copy_to(prefix+'-before.h264', seconds=10)
+                    record = io.BytesIO()
+                    stream.copy_to(record, seconds=10)
                     stream.clear()
+                    camera.split_recording(record)
+                    wait(camera, 3)
                     # Wait until motion is no longer detected, then split
                     # recording back to the in-memory circular buffer
-                    wait(camera, 3)
                     while detect_motion(camera):
                         wait(camera, 3)
                     print('Motion stopped!')
                     camera.split_recording(stream)
+                    record.seek(0)
+                    conn.storeFile(share.name, 'test/'+ts_str()+'.h264', record)
                 else:
                     print('No motion')
         finally:
+            conn.close()
             camera.stop_recording()
 
